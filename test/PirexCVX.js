fcfb48144f56789ef7a29d6b4d193ab91831901b
Khoa Phan <khoaphan89@gmail.com>
2022-02-03 14:13:52 -0600

    Add method for locking CVX

--- test/PirexCVX.js
+++ test/PirexCVX.js
@@ -10,9 +10,7 @@ describe("PirexCVX", () => {
   before(async () => {
     [admin, notAdmin] = await ethers.getSigners();
 
-    cvx = await (
-      await ethers.getContractFactory("Cvx")
-    ).deploy();
+    cvx = await (await ethers.getContractFactory("Cvx")).deploy();
 
     cvxLocker = await (
       await ethers.getContractFactory("CvxLocker")
@@ -22,7 +20,9 @@ describe("PirexCVX", () => {
       await ethers.getContractFactory("PirexCVX")
     ).deploy(cvxLocker.address, cvx.address);
 
-    await cvxLocker.setStakingContract('0xe096ccec4a1d36f191189fe61e803d8b2044dfc3');
+    await cvxLocker.setStakingContract(
+      "0xe096ccec4a1d36f191189fe61e803d8b2044dfc3"
+    );
     await cvxLocker.setApprovals();
 
     await cvx.mint(admin.address, initialCvxBalanceForAdmin);
@@ -37,6 +37,47 @@ describe("PirexCVX", () => {
       expect(owner).to.equal(admin.address);
       expect(_cvxLocker).to.equal(cvxLocker.address);
       expect(_cvx).to.equal(cvx.address);
+    });
+  });
+
+  describe("deposit", () => {
+    it("Should deposit CVX", async () => {
+      const cvxBalanceBeforeDeposit = await cvx.balanceOf(admin.address);
+      const vlCvxBalanceBeforeDeposit = await cvxLocker.balanceOf(
+        admin.address
+      );
+      const lockAmount = ethers.BigNumber.from(`${1e18}`);
+      const spendRatio = 0;
+
+      await cvx.approve(pirexCvx.address, lockAmount);
+
+      const { events } = await (
+        await pirexCvx.lock(admin.address, lockAmount, spendRatio)
+      ).wait();
+      const lockEvent = events[events.length - 1];
+      const rewardsDuration = Number(
+        (await cvxLocker.rewardsDuration()).toString()
+      );
+
+      // Fast forward 1 rewards duration so that balance is reflected
+      await ethers.provider.send("evm_increaseTime", [rewardsDuration]);
+      await network.provider.send("evm_mine");
+
+      const cvxBalanceAfterDeposit = await cvx.balanceOf(admin.address);
+      const vlCvxBalanceAfterDeposit = await cvxLocker.balanceOf(admin.address);
+
+      expect(cvxBalanceAfterDeposit).to.equal(
+        cvxBalanceBeforeDeposit.sub(lockAmount)
+      );
+      expect(vlCvxBalanceAfterDeposit).to.equal(
+        vlCvxBalanceBeforeDeposit.add(lockAmount)
+      );
+      expect(lockEvent.eventSignature).to.equal(
+        "Lock(address,uint256,uint256)"
+      );
+      expect(lockEvent.args.account).to.equal(admin.address);
+      expect(lockEvent.args.amount).to.equal(lockAmount);
+      expect(lockEvent.args.spendRatio).to.equal(spendRatio);
     });
   });
 });
