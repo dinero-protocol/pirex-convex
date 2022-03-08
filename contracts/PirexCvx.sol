12d2b3b8c2f22ed12e849409f3bd603c8b564725
Khoa Phan <khoaphan89@gmail.com>
2022-03-08 09:55:38 -0600

    Add setters

--- contracts/PirexCvx.sol
+++ contracts/PirexCvx.sol
@@ -20,8 +20,14 @@ contract PirexCvx is Ownable, ERC20("Pirex CVX", "pCVX") {
 
     bytes32 public delegationSpace = bytes32(bytes("cvx.eth"));
 
+    event SetCvx(address _cvx);
+    event SetCvxLocker(address _cvxLocker);
+    event SetCvxDelegateRegistry(address _cvxDelegateRegistry);
+    event SetDelegationSpace(string _delegationSpace);
+
     error ZeroAddress();
     error ZeroAmount();
+    error EmptyString();
 
     /**
         @param  _cvx                     address     CVX address    
@@ -41,5 +47,56 @@ contract PirexCvx is Ownable, ERC20("Pirex CVX", "pCVX") {
 
         if (_cvxDelegateRegistry == address(0)) revert ZeroAddress();
         cvxDelegateRegistry = ICvxDelegateRegistry(_cvxDelegateRegistry);
+    }
+
+    /** 
+        @notice Set CVX token address
+        @param  _cvx  address  CVX address    
+     */
+    function setCvx(address _cvx) external onlyOwner {
+        if (_cvx == address(0)) revert ZeroAddress();
+        cvx = ERC20(_cvx);
+
+        emit SetCvx(_cvx);
+    }
+
+    /** 
+        @notice Set CvxLocker address
+        @param  _cvxLocker  address  CvxLocker address    
+     */
+    function setCvxLocker(address _cvxLocker) external onlyOwner {
+        if (_cvxLocker == address(0)) revert ZeroAddress();
+        cvxLocker = ICvxLocker(_cvxLocker);
+
+        emit SetCvxLocker(_cvxLocker);
+    }
+
+    /** 
+        @notice Set CvxDelegateRegistry address
+        @param  _cvxDelegateRegistry  address  CvxDelegateRegistry address    
+     */
+    function setCvxDelegateRegistry(address _cvxDelegateRegistry)
+        external
+        onlyOwner
+    {
+        if (_cvxDelegateRegistry == address(0)) revert ZeroAddress();
+        cvxDelegateRegistry = ICvxDelegateRegistry(_cvxDelegateRegistry);
+
+        emit SetCvxDelegateRegistry(_cvxDelegateRegistry);
+    }
+
+    /** 
+        @notice Set delegationSpace
+        @param  _delegationSpace  string  Convex Snapshot delegation space
+     */
+    function setDelegationSpace(string memory _delegationSpace)
+        external
+        onlyOwner
+    {
+        bytes memory d = bytes(_delegationSpace);
+        if (d.length == 0) revert EmptyString();
+        delegationSpace = bytes32(d);
+
+        emit SetDelegationSpace(_delegationSpace);
     }
 }
