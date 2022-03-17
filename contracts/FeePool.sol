d51f7d31beb878c091361e35ac8432d0cf2725c8
kphed <kpcrypto69@gmail.com>
2022-03-17 14:23:42 -0500

    Distribute reward fees

--- contracts/FeePool.sol
+++ contracts/FeePool.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.12;
 
+import "hardhat/console.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
