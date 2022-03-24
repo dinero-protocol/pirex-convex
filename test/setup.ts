65b71e5a64ec366699f73df7015f2c537b79a76f
Eko Mirhard <eko.mirhard@gmail.com>
2022-03-24 19:41:27 +0700

    Relocate setupConvex logic into the main before hook

--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,6 @@
 import { ethers } from 'hardhat';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-import { setUpConvex } from './helpers';
+import { toBN } from './helpers';
 import {
   ConvexToken,
   CvxLocker,
@@ -10,6 +10,7 @@ import {
   Crv,
   PirexFees,
   CvxRewardPool,
+  AddressRegistry,
 } from '../typechain-types';
 
 let admin: SignerWithAddress;
@@ -25,20 +26,91 @@ let cvxCrvToken: any;
 let cvxLocker: CvxLocker;
 let cvxDelegateRegistry: DelegateRegistry;
 let cvxRewardPool: CvxRewardPool;
+let votiumAddressRegistry: AddressRegistry;
 let votiumMultiMerkleStash: MultiMerkleStash;
 
 before(async function () {
   [admin, notAdmin, treasury, revenueLockers, contributors] =
     await ethers.getSigners();
-  ({
-    cvx,
-    crv,
-    cvxCrvToken,
-    cvxLocker,
-    cvxRewardPool,
-    cvxDelegateRegistry,
-    votiumMultiMerkleStash,
-  } = await setUpConvex());
+
+  // Constants
+  const initialBalanceForAdmin = toBN(100e18);
+  const crvAddr = '0xd533a949740bb3306d119cc777fa900ba034cd52';
+  const crvDepositorAddr = '0x8014595F2AB54cD7c604B00E9fb932176fDc86Ae';
+  const cvxCrvRewardsAddr = '0x3Fe65692bfCD0e6CF84cB1E7d24108E434A7587e';
+  const cvxCrvTokenAddr = '0x62B9c7356A2Dc64a1969e19C23e4f579F9810Aa7';
+
+  // Contracts
+  const curveVoterProxy = await (
+    await ethers.getContractFactory('CurveVoterProxy')
+  ).deploy();
+  cvx = await (
+    await ethers.getContractFactory('ConvexToken')
+  ).deploy(curveVoterProxy.address);
+  crv = await (await ethers.getContractFactory('Crv')).deploy();
+  cvxCrvToken = await (await ethers.getContractFactory('cvxCrvToken')).deploy();
+  const booster = await (
+    await ethers.getContractFactory('Booster')
+  ).deploy(curveVoterProxy.address, cvx.address);
+  const rewardFactory = await (
+    await ethers.getContractFactory('RewardFactory')
+  ).deploy(booster.address);
+  const baseRewardPool = await (
+    await ethers.getContractFactory(
+      'contracts/mocks/BaseRewardPool.sol:BaseRewardPool'
+    )
+  ).deploy(
+    0,
+    cvxCrvToken.address,
+    crv.address,
+    booster.address,
+    rewardFactory.address
+  );
+  cvxLocker = await (
+    await ethers.getContractFactory('CvxLocker')
+  ).deploy(cvx.address, cvxCrvToken.address, baseRewardPool.address);
+  cvxRewardPool = await (
+    await ethers.getContractFactory('CvxRewardPool')
+  ).deploy(
+    cvx.address,
+    crvAddr,
+    crvDepositorAddr,
+    cvxCrvRewardsAddr,
+    cvxCrvTokenAddr,
+    booster.address,
+    admin.address
+  );
+  const cvxStakingProxy = await (
+    await ethers.getContractFactory('CvxStakingProxy')
+  ).deploy(
+    cvxLocker.address,
+    cvxRewardPool.address,
+    crv.address,
+    cvx.address,
+    cvxCrvToken.address
+  );
+
+  // Workaround for Typescript not acknowledging deploy method
+  const v: any = await ethers.getContractFactory('MultiMerkleStash');
+  votiumMultiMerkleStash = await v.deploy();
+
+  votiumAddressRegistry = await (
+    await ethers.getContractFactory('AddressRegistry')
+  ).deploy();
+  cvxDelegateRegistry = await (
+    await ethers.getContractFactory('DelegateRegistry')
+  ).deploy();
+
+  // Configurations
+  await cvxLocker.setStakingContract(cvxStakingProxy.address);
+  await cvxLocker.setApprovals();
+  await cvxLocker.addReward(crv.address, admin.address, true);
+  await cvxLocker.addReward(cvxCrvToken.address, admin.address, true);
+  await cvxStakingProxy.setApprovals();
+  await cvx.mint(admin.address, initialBalanceForAdmin);
+  await crv.mint(admin.address, initialBalanceForAdmin);
+  await cvxCrvToken.mint(admin.address, initialBalanceForAdmin);
+
   pirexFees = await (
     await ethers.getContractFactory('PirexFees')
   ).deploy(treasury.address, revenueLockers.address, contributors.address);
@@ -65,6 +137,7 @@ before(async function () {
   this.cvxLocker = cvxLocker;
   this.cvxRewardPool = cvxRewardPool;
   this.cvxDelegateRegistry = cvxDelegateRegistry;
+  this.votiumAddressRegistry = votiumAddressRegistry;
   this.votiumMultiMerkleStash = votiumMultiMerkleStash;
   this.pirexFees = pirexFees;
   this.pCvx = pCvx;
