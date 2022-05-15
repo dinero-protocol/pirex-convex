4680466b3fa887d99ba4edd1b96539f98399ea65
kphed <kpcrypto69@gmail.com>
2022-05-15 09:18:40 -0500

    Add clarifying comments for problematic method

--- contracts/mocks/UnionPirexStrategyMock.sol
+++ contracts/mocks/UnionPirexStrategyMock.sol
@@ -17,21 +17,31 @@ contract UnionPirexStrategyMock is UnionPirexStrategy {
         address _vault
     ) UnionPirexStrategy(_pirexCvx, _pxCVX, _distributor, _vault) {}
 
+    /**
+        @notice Problematic version of `notifyRewardAmount` susceptible to erroneous 
+                reward distribution if not called correctly by the distributor.
+     */
     function notifyRewardAmountProblematic()
         external
         onlyDistributor
         updateReward(address(0))
     {
+        // @NOTE: Comments pre-fixed with NOTE will detail issues while preserving code
+
         // Rewards transferred directly to this contract are not added to _totalSupply
         // To get the rewards w/o relying on a potentially incorrect passed in arg,
         // we can use the difference between the token balance and _totalSupply
         uint256 reward = token.balanceOf(address(this)) - _totalSupply;
 
         if (block.timestamp >= periodFinish) {
+            // @NOTE: Earned, unclaimed rewards they will be doubly distributed if not deducted
             rewardRate = reward / rewardsDuration;
         } else {
             uint256 remaining = periodFinish - block.timestamp;
             uint256 leftover = remaining * rewardRate;
+
+            // @NOTE: If the new reward amount is not singled out before adding leftovers, then
+            // any leftovers and earned, unclaimed rewards will be doubly distributed
             rewardRate = (reward + leftover) / rewardsDuration;
         }
 
@@ -39,6 +49,7 @@ contract UnionPirexStrategyMock is UnionPirexStrategy {
         // This keeps the reward rate in the right range, preventing overflows due to
         // very high values of rewardRate in the earned and rewardsPerToken functions;
         // Reward + leftover must be less than 2^256 / 10^18 to avoid overflow.
+        // @NOTE: This check is insufficient as it should not include the non-reward balance
         uint256 balance = token.balanceOf(address(this));
         require(
             rewardRate <= balance / rewardsDuration,
