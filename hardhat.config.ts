75b6b3c6f1d5e6f654b6389f82125800ef141db1
kphed <kpcrypto69@gmail.com>
2022-05-29 06:00:38 -0500

    Add forge test for setting Votum contract and update optimizer runs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -66,7 +66,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 99,
+            runs: 179,
           },
         },
       },
